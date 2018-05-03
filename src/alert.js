f7fe8be9f4081dc85484cd90ff9e8ac714bc1c15
KimHeeCheol <msn9975@gmail.com>
2018-05-03 21:05:08 +0900

    alert reload

---
+++ src/alert.js
@@ -0,0 +1,14 @@
+(function () {
+  var 변수명 = document.getElementById('??');
+  
+  변수명.addEventListener('click', function() {
+  
+  });
+  var register = document.getElementById('register');
+  register.addEventListener('click', function() {
+    alert('등록 되었습니다');
+    location.reload();
+  });
+})();
+
+
