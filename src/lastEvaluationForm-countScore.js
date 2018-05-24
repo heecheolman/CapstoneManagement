48b3f34f3114a4d3718b6a8835192127f50cb2fd
KimHeeCheol <msn9975@gmail.com>
2018-05-24 21:19:55 +0900

    수정

---
+++ src/lastEvaluationForm-countScore.js
@@ -0,0 +1,10 @@
+(function() {
+  var score = document.getElementById('result');
+  var sum = 0;
+  var radio = document.querySelectorAll('input[name="val_1"]');
+  for(var i = 0; i < 5; i++) {
+    radio[i].addEventListener('click', function() {
+      score.innerHTML = parseInt(document.querySelector('input[name="val_1"]:checked').value);
+    });
+  }
+})();
