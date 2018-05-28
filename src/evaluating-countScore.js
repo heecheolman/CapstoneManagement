c99c3496d8163900a8131dfc1de09cd6ad66a6bf
KimHeeCheol <msn9975@gmail.com>
2018-05-28 20:39:49 +0900

    수정수정수정수정

--- src/evaluating-countScore.js
+++ src/evaluating-countScore.js
@@ -1,20 +1,10 @@
-(function() {
-  var score = document.getElementById('result');
-  console.log(score);
-  var sum = 0;
-  for(var i = 0; i < 7; i++) {
-    sum += parseInt(document.querySelector('input[name="val_'+ (i + 1) + '"]:checked').value);
-  }
-  score.innerHTML = sum;
-})();
-
 (function () {
   var sumList = [];
   sumList.length = 7;
-  // 초기화
-  for(var i = 0; i < sumList.length; i++) {
-    sumList[i] = 0;
-  }
+  
+  // 처음 값을 불러오기위한 호출
+  sum();
+  
   var radio = [];
   radio.length = 7;
   
@@ -22,55 +12,21 @@
     radio[i] = document.querySelectorAll('input[name="val_' + (i + 1) + '"]');
   }
   
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[0][i].addEventListener('click', function() {
-      sumList[0] = document.querySelector('input[name="val_1"]:checked').value;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[1][i].addEventListener('click', function() {
-      sumList[1] = document.querySelector('input[name="val_2"]:checked').value;;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[2][i].addEventListener('click', function() {
-      sumList[2] = document.querySelector('input[name="val_3"]:checked').value;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[3][i].addEventListener('click', function() {
-      sumList[3] = document.querySelector('input[name="val_4"]:checked').value;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[4][i].addEventListener('click', function() {
-      sumList[4] = document.querySelector('input[name="val_5"]:checked').value;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[5][i].addEventListener('click', function() {
-      sumList[5] = document.querySelector('input[name="val_6"]:checked').value;
-      sum();
-    });
-  }
-  for(var i = 0; i < radio[0].length; i++) {
-    radio[6][i].addEventListener('click', function() {
-      sumList[6] = document.querySelector('input[name="val_7"]:checked').value;
-      sum();
-    });
+  for(var i = 0; i < radio.length; i++) {
+    for(var j = 0; j < radio[0].length; j++) {
+      radio[i][j].addEventListener('click', function() {
+        sum();
+      });
+    }
   }
   
   function sum() {
     var score = document.getElementById('result');
     var sum = 0;
     for(var i = 0; i < sumList.length; i++) {
-      sum += parseInt(sumList[i]);
+      sumList[i] = parseInt(document.querySelector('input[name="val_' + (i + 1) + '"]:checked').value);
+      sum += sumList[i];
     }
-    score.innerHTML = sum;
+    score.innerHTML = parseInt(sum);
   }
 })();
