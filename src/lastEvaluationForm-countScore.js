f50e857b0dd7baadfc330ea470e0b46ee30547a4
KimHeeCheol <msn9975@gmail.com>
2018-05-28 10:07:46 +0900

    수정사항

--- src/lastEvaluationForm-countScore.js
+++ src/lastEvaluationForm-countScore.js
@@ -1,6 +1,11 @@
 (function() {
   var score = document.getElementById('result');
   var sum = 0;
+})();
+
+(function() {
+  var score = document.getElementById('result');
+  var sum = 0;
   var radio = document.querySelectorAll('input[name="val_1"]');
   for(var i = 0; i < 5; i++) {
     radio[i].addEventListener('click', function() {
